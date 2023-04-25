5e226917fbb1dfd3c8e9fbcd69195959169e702b
J.Alberto Azevedo <alazib7@gmail.com>
2023-04-25 18:49:39 +0200

    warning avoided

--- config/mongo.js
+++ config/mongo.js
@@ -2,6 +2,9 @@ const mongoose = require("mongoose")
 
 const dbConnect = () => {
   const DB_URI = process.env.DB_URI
+  //this mongoos.set is to avoid this warnin: (node:22160) [MONGOOSE] DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7. Use `mongoose.set('strictQuery', false);` if you want to prepare for this change. Or use `mongoose.set('strictQuery', true);` to suppress this warning.
+  mongoose.set("strictQuery", false)
+
   mongoose
     .connect(DB_URI, {
       useNewUrlParser: true,
