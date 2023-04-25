94602a848a50b3d025f5c5e39ea17e3eadb80b31
J.Alberto Azevedo <alazib7@gmail.com>
2023-04-25 18:51:54 +0200

    notes added

--- config/mongo.js
+++ config/mongo.js
@@ -2,9 +2,10 @@ const mongoose = require("mongoose")
 
 const dbConnect = () => {
   const DB_URI = process.env.DB_URI
-  //this mongoos.set is to avoid this warnin: (node:22160) [MONGOOSE] DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7. Use `mongoose.set('strictQuery', false);` if you want to prepare for this change. Or use `mongoose.set('strictQuery', true);` to suppress this warning.
+  //this mongoose.set is to avoid this warning: (node:22160) [MONGOOSE] DeprecationWarning: Mongoose: the `strictQuery` option will be switched back to `false` by default in Mongoose 7. Use `mongoose.set('strictQuery', false);` if you want to prepare for this change. Or use `mongoose.set('strictQuery', true);` to suppress this warning.
   mongoose.set("strictQuery", false)
 
+  //this mongoose.connect connects the API with de mongoDB-Atlas Are You Talking To Me DDBB
   mongoose
     .connect(DB_URI, {
       useNewUrlParser: true,
